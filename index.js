0b07cc95941f3e67d20ef68b1053cd6e47a4b538
roccohia <yurongxin@yahoo.com>
2025-06-30 19:28:50 +0800

    🎉 Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`; 
+
+const chat_id = update.message.chat.id;
+const text = update.message.text.trim();
+console.log(`[Telegram] 收到消息:`, text); 
